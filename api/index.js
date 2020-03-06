c3cd6c4bbcb7bab46b8b05dda515074f55bcdbf5
kingeunji <kokopink12@gmail.com>
2020-03-06 21:44:46 +0900

    Add [Admin] 블로그 글 업로드 API 연동 성공

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const domain = 'http://52.78.126.28:8080/v1/api/'
+const Unauthorized = 401
+
+const request = (method, url, data) => {
+    return axios({
+      method, 
+      url: domain + url, 
+      data
+    }).then(result => result.data)
+      .catch(result => {
+        const {status} = result.response
+        if (status === Unauthorized) 
+            // 로그인 화면으로 
+        throw result.response
+      })
+}
+
+export const category = {
+    fetch() {
+        return request('GET', 'categorys')
+    },
+    create(category) {
+        return request('POST', 'categorys', category)
+    }
+}
+
+export const post = {
+    create(form) {
+        return request('POST', 'posts', form)
+    }
+}
